113e51b33c66c120b7cf021afca191dc84e2da1c
Richard42Graham <up628887@myport.ac.uk>
2020-03-27 18:21:18 +0100

    lines now connect

--- AI.py
+++ AI.py
@@ -7,14 +7,14 @@ import math
 def main(name_ofSVG):
 
     canvus_height = 100              # max height of the "paper"
-
+    add_circles_to_points = True
     lines = list()
     lines.append(generate_points(0, 0, 20, canvus_height, 2, 10, 2, 5))
     lines.append(generate_points(10, 0, 20, canvus_height, 2, 10, 2, 5))
     lines.append(generate_points(20, 0, 20, canvus_height, 2, 10, 2, 10))
     lines.append(generate_points(30, 0, 20, canvus_height, 2, 10, 2, 10))
     lines.append(generate_points(40, 0, 20, canvus_height, 2, 10, 2, 10))
-    render_svg(lines, name_ofSVG)
+    render_svg(lines, add_circles_to_points, name_ofSVG)
 
 
 def generate_points(
@@ -71,38 +71,38 @@ def get_next_x(min_line_lenght, max_line_lenght, x_current):
     return x_new
 
 
-def render_svg(lines, svg_output_file):
+def render_svg(lines, add_circles_to_points, svg_output_file):
     dwg = svgwrite.Drawing(filename=svg_output_file, debug=True)
     # Rendering goes here
+    toCm = 35.43307  # https://github.com/mozman/svgwrite/blob/master/doc/overview.rst
 
     dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'),
                      rx=None, ry=None, fill='rgb(0,0,5)'))
-
-    vlines = dwg.add(dwg.g(id='vline', stroke='blue', stroke_width=20))
-
+   #print("({},{}) ({},{})".format(first_x, first_y, last_x, last_y))
     for points in lines:
-        for point_index in range((len(points) - 1)):
+        polyline = dwg.polyline(id='vline', stroke='blue',
+                                fill_opacity=0, stroke_width=10)
+        for point_index in range((len(points))):
             point = points[point_index]
             first_x = point[0]
             first_y = point[1]
+            polyline.points.append((first_x*toCm, first_y*toCm))
+        dwg.add(polyline)
 
-            point = points[point_index + 1]
-            last_x = point[0]
-            last_y = point[1]
-
-            print("({},{}) ({},{})".format(first_x, first_y, last_x, last_y))
-
-            # Polyline.points.append( point )
-
-            vlines.add(dwg.line(start=(first_x*cm, first_y*cm),
-                                end=(last_x*cm, last_y*cm)))
-            #circle =dwg.circle(center=(first_x*cm, first_y*cm), r='0.5cm', stroke='blue', stroke_width=3,fill='blue')
-            #circle['class'] = 'class1 class2'
-            #dwg.add(circle)
-
-            #circle =dwg.circle(center=(first_x*cm, first_y*cm), r='0.4cm', stroke='black', stroke_width=3,fill='black')
-            #circle['class'] = 'class1 class2'
-            #dwg.add(circle)
+    if add_circles_to_points:
+        for points in lines:
+            for point_index in range((len(points))):
+                point = points[point_index]
+                first_x = point[0]
+                first_y = point[1]
+                circle = dwg.circle(center=(first_x*toCm, first_y*toCm),
+                                    r='0.5cm', stroke='blue', stroke_width=3, fill='blue')
+                circle['class'] = 'class1 class2'
+                dwg.add(circle)
+                circle = dwg.circle(center=(first_x*toCm, first_y*toCm),
+                                    r='0.3cm', stroke='black', stroke_width=3, fill='black')
+                circle['class'] = 'class1 class2'
+                dwg.add(circle)
 
     dwg.save()
 
